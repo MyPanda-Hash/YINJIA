4960bc04483d1538ab43fe05c29943ca766eaaf5
MyPanda-Hash <MyPanda-Hash@users.noreply.github.com>
2026-09-04 14:12:29 +0800

    feat: 文书面板右上角改显示单据编号(实施计划单号,引擎流水唯一,只读不可改)

--- frontend/src/core/views/DocSheet.vue
+++ frontend/src/core/views/DocSheet.vue
@@ -12,17 +12,8 @@
     <!-- ① 顶部条 -->
     <div class="as-topbar">
       <div class="as-company">惠州市银嘉环保科技有限公司</div>
-      <div class="as-docno">
-        <el-input
-          v-if="editable"
-          v-model="head['文档编号']"
-          size="small"
-          maxlength="30"
-          class="as-docno-input"
-          @input="emit('dirty')"
-        />
-        <template v-else>{{ head['文档编号'] || 'YJ-XS002' }}</template>
-      </div>
+      <!-- 右上角显示单据编号(实施计划单号):引擎流水号唯一,只读不可改 -->
+      <div class="as-docno">{{ head['单据编号'] || head['文档编号'] || 'YJ-XS002' }}</div>
     </div>
 
     <!-- ② 标题行:大标题 + 右上信息表 -->
@@ -280,19 +271,6 @@ function selectOptions(key) {
   color: #333;
   text-align: right;
   padding: 8px 14px 0;
-  display: flex;
-  justify-content: flex-end;
-  align-items: flex-start;
-}
-.as-docno-input {
-  width: 130px;
-}
-.as-docno-input :deep(.el-input__inner) {
-  text-align: right;
-  font-family: 'KaiTi', 'STKaiti', 'SimSun', serif;
-  font-style: italic;
-  font-size: 14px;
-  padding: 0;
 }
 
 /* ═══ ② 标题行:大标题 + 信息表 ═══ */
