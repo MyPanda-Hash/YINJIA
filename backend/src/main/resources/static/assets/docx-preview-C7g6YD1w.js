d3dd6b3c31e745ffe01cf10e19460aade9010831
MyPanda-Hash <MyPanda-Hash@users.noreply.github.com>
2026-09-22 15:51:09 +0800

    ﻿build: 前端产物重建(我的桌面展示打磨进入已发布包)

--- backend/src/main/resources/static/assets/docx-preview-C7g6YD1w.js
+++ backend/src/main/resources/static/assets/docx-preview-C7g6YD1w.js
@@ -1,4 +1,4 @@
-import{aV as zt,h as _e}from"./index-DJ3SI2Z4.js";function Rt(l){throw new Error('Could not dynamically require "'+l+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var jt={exports:{}};/*!
+import{aV as zt,h as _e}from"./index-jU_PdnLv.js";function Rt(l){throw new Error('Could not dynamically require "'+l+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var jt={exports:{}};/*!
 
 JSZip v3.10.2 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
